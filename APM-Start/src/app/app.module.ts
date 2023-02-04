3eed453748d022cb6850cf3920c72498a6acef4b
mahmoudelmekwed <mahmoud.elmekwed@gmail.com>
2023-02-04 04:18:40 +0200

    custom pipes

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import { ConvetToSpacesPipe } from './shared/convert-to-spaces.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConvetToSpacesPipe
   ],
   imports: [
     BrowserModule,
