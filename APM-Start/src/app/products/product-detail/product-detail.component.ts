80b248f59d86342914da0e180d7fde284d008bf7
mahmoudelmekwed <mahmoud.elmekwed@gmail.com>
2023-02-07 22:22:11 +0200

    product detail

--- APM-Start/src/app/products/product-detail/product-detail.component.ts
+++ APM-Start/src/app/products/product-detail/product-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { IProduct } from '../product';
+import { ProductService } from '../product.servise';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -11,11 +12,19 @@ export class ProductDetailComponent implements OnInit {
   product: IProduct | undefined;
 
   constructor(private route : ActivatedRoute
-    ,private router : Router) { }
+    ,private router : Router,
+    private productservice: ProductService) { }
 
   ngOnInit(): void {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.pageTitle += `: ${id}`;
+    // const id = Number(this.route.snapshot.paramMap.get('id'));
+    // this.pageTitle += `: ${id}`;
+
+    this.route.params.subscribe((params)=>{
+      this.productservice.getProducts().subscribe((res)=>
+      this.product = res.find(
+        (item)=> item.productId == params['id']
+      ))
+    })
     
   }
 
